cefcbd96ffd21946b26ace76ea7368ac3752bfdc
Oliver <jasper.kontturi@gmail.com>
2022-12-22 22:14:49 +0200

    Marketplace Configuration Packets

---
+++ src/Skylight.Protocol.X/Skylight.Protocol.WIN63-202111081545-75921380/Packets/Parsers/Marketplace/GetMarketplaceConfigurationPacketParser.cs
@@ -0,0 +1,22 @@
+﻿using System.Buffers;
+using Skylight.Protocol.Extensions;
+using Skylight.Protocol.Packets.Incoming.Marketplace;
+using Net.Buffers;
+using Net.Communication.Attributes;
+using Net.Communication.Incoming.Parser;
+
+namespace Skylight.Protocol.WIN63_202111081545_75921380.Packets.Parsers.Marketplace;
+
+[PacketParserId(116u)]
+[PacketManagerRegister(typeof(GamePacketManager))]
+internal sealed class GetMarketplaceConfigurationPacketParser : IIncomingPacketParser<GetMarketplaceConfigurationPacketParser.GetMarketplaceConfigurationIncomingPacket>
+{
+	public GetMarketplaceConfigurationIncomingPacket Parse(ref PacketReader reader)
+	{
+		return new GetMarketplaceConfigurationIncomingPacket();
+	}
+
+	internal readonly struct GetMarketplaceConfigurationIncomingPacket : IGetMarketplaceConfigurationIncomingPacket
+	{
+	}
+}
---
+++ src/Skylight.Protocol/Packets/Outgoing/Marketplace/MarketplaceConfigurationOutgoingPacket.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Skylight.Protocol.Packets.Outgoing.Marketplace;
+
+public sealed class MarketplaceConfigurationOutgoingPacket : IGameOutgoingPacket
+{
+	public required bool Enabled { get; init; }
+	public required int Commission { get; init; }
+	public required int TokenOfferPrice { get; init; }
+	public required int TokenOfferAmount { get; init; }
+	public required int OfferMinPrice { get; init; }
+	public required int OfferMaxPrice { get; init; }
+	public required int OfferExpirationHours { get; init; }
+	public required int ItemPricePeriod { get; init; }
+	public required int SellingFeePercentage { get; init; }
+	public required int RevenueLimit { get; init; }
+	public required int HalfTaxLimit { get; init; }
+
+	public MarketplaceConfigurationOutgoingPacket()
+	{
+	}
+
+	[SetsRequiredMembers]
+	public MarketplaceConfigurationOutgoingPacket(bool enabled, int commission, int tokenOfferPrice, int tokenOfferAmount, int offerMinPrice, int offerMaxPrice, int offerExpirationHours, int itemPricePeriod, int sellingFeePercentage, int revenueLimit, int halfTaxLimit)
+	{
+		this.Enabled = enabled;
+		this.Commission = commission;
+		this.TokenOfferPrice = tokenOfferPrice;
+		this.TokenOfferAmount = tokenOfferAmount;
+		this.OfferMinPrice = offerMinPrice;
+		this.OfferMaxPrice = offerMaxPrice;
+		this.OfferExpirationHours = offerExpirationHours;
+		this.ItemPricePeriod = itemPricePeriod;
+		this.SellingFeePercentage = sellingFeePercentage;
+		this.RevenueLimit = revenueLimit;
+		this.HalfTaxLimit = halfTaxLimit;
+	}
+}
