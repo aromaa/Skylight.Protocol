d8db664fef7ac0e3d370bb7c34569213e262caa7
Joni Aromaa <me@joniaromaa.fi>
2022-12-22 22:43:57 +0200

    Remove unnecessary using's

--- src/Skylight.Protocol/Packets/Incoming/Marketplace/IGetMarketplaceConfigurationIncomingPacket.cs
+++ src/Skylight.Protocol/Packets/Incoming/Marketplace/IGetMarketplaceConfigurationIncomingPacket.cs
@@ -1,10 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Skylight.Protocol.Attributes;
-
 namespace Skylight.Protocol.Packets.Incoming.Marketplace;
 
 public interface IGetMarketplaceConfigurationIncomingPacket : IGameIncomingPacket
