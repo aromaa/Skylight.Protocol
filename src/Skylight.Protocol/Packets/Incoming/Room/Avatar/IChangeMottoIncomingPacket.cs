a4d510721daa7ca4154f954f9120c1ff846063a9
Oliver <37178886+OliverSpeed@users.noreply.github.com>
2023-04-19 20:41:55 +0300

    Update IChangeMottoIncomingPacket.cs

--- src/Skylight.Protocol/Packets/Incoming/Room/Avatar/IChangeMottoIncomingPacket.cs
+++ src/Skylight.Protocol/Packets/Incoming/Room/Avatar/IChangeMottoIncomingPacket.cs
@@ -1,6 +1,7 @@
 ﻿using System.Buffers;
 
 namespace Skylight.Protocol.Packets.Incoming.Room.Avatar;
+
 public interface IChangeMottoIncomingPacket : IGameIncomingPacket
 {
 	public ReadOnlySequence<byte> Motto { get; }
