0f8a50f02d6851c36653ea4101747606135ca29f
Oliver <jasper.kontturi@gmail.com>
2023-04-20 01:53:19 +0300

    Home room

---
+++ src/Skylight.Protocol.X/Skylight.Protocol.WIN63-202111081545-75921380/Packets/Parsers/Navigator/UpdateHomeRoomPacketParser.cs
@@ -0,0 +1,26 @@
+﻿using System.Buffers;
+using Skylight.Protocol.Extensions;
+using Skylight.Protocol.Packets.Incoming.Navigator;
+using Net.Buffers;
+using Net.Communication.Attributes;
+using Net.Communication.Incoming.Parser;
+
+namespace Skylight.Protocol.WIN63_202111081545_75921380.Packets.Parsers.Navigator;
+
+[PacketParserId(2116u)]
+[PacketManagerRegister(typeof(GamePacketManager))]
+internal sealed class UpdateHomeRoomPacketParser : IIncomingPacketParser<UpdateHomeRoomPacketParser.UpdateHomeRoomIncomingPacket>
+{
+	public UpdateHomeRoomIncomingPacket Parse(ref PacketReader reader)
+	{
+		return new UpdateHomeRoomIncomingPacket
+		{
+			FlatId = reader.ReadInt32()
+		};
+	}
+
+	internal readonly struct UpdateHomeRoomIncomingPacket : IUpdateHomeRoomIncomingPacket
+	{
+		public int FlatId { get; init; }
+	}
+}
