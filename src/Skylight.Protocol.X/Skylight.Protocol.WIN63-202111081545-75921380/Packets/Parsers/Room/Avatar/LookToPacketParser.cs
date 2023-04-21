bfa97456b733c69b0180a7f6dedc17f877849a00
Josh <Diddyy@users.noreply.github.com>
2023-04-22 00:52:40 +0100

    Add LookTo Packet (#6)

---
+++ src/Skylight.Protocol.X/Skylight.Protocol.WIN63-202111081545-75921380/Packets/Parsers/Room/Avatar/LookToPacketParser.cs
@@ -0,0 +1,28 @@
+﻿using System.Buffers;
+using Skylight.Protocol.Extensions;
+using Skylight.Protocol.Packets.Incoming.Room.Avatar;
+using Net.Buffers;
+using Net.Communication.Attributes;
+using Net.Communication.Incoming.Parser;
+
+namespace Skylight.Protocol.WIN63_202111081545_75921380.Packets.Parsers.Room.Avatar;
+
+[PacketParserId(3605u)]
+[PacketManagerRegister(typeof(GamePacketManager))]
+internal sealed class LookToPacketParser : IIncomingPacketParser<LookToPacketParser.LookToIncomingPacket>
+{
+	public LookToIncomingPacket Parse(ref PacketReader reader)
+	{
+		return new LookToIncomingPacket
+		{
+			X = reader.ReadInt32(),
+			Y = reader.ReadInt32()
+		};
+	}
+
+	internal readonly struct LookToIncomingPacket : ILookToIncomingPacket
+	{
+		public int X { get; init; }
+		public int Y { get; init; }
+	}
+}
---
+++ src/Skylight.Protocol/Packets/Incoming/Room/Avatar/ILookToIncomingPacket.cs
@@ -0,0 +1,7 @@
+﻿namespace Skylight.Protocol.Packets.Incoming.Room.Avatar;
+
+public interface ILookToIncomingPacket : IGameIncomingPacket
+{
+	public int X { get; }
+	public int Y { get; }
+}
