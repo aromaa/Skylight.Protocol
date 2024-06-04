96a5d8bccd3cd079704d77ef64a853ce20ef9203
Oliver <jasper.kontturi@gmail.com>
2024-06-04 08:20:56 +0300

    Profieles

---
+++ src/Skylight.Protocol/Packets/Outgoing/Catalog/BonusRareInfoOutgoingPacket.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Skylight.Protocol.Packets.Outgoing.Catalog;
+
+public sealed class BonusRareInfoOutgoingPacket : IGameOutgoingPacket
+{
+	public required string ProductType { get; init; }
+	public required int ProductClassId { get; init; }
+	public required int TotalCoinsForBonus { get; init; }
+	public required int CoinsStillRequiredToBuy { get; init; }
+
+	public BonusRareInfoOutgoingPacket()
+	{
+	}
+
+	[SetsRequiredMembers]
+	public BonusRareInfoOutgoingPacket(string productType, int productClassId, int totalCoinsForBonus, int coinsStillRequiredToBuy)
+	{
+		this.ProductType = productType;
+		this.ProductClassId = productClassId;
+		this.TotalCoinsForBonus = totalCoinsForBonus;
+		this.CoinsStillRequiredToBuy = coinsStillRequiredToBuy;
+	}
+}
