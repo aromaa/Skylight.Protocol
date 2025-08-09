c6e030faf7369133a4763845f6627d91611b53b8
Oliver <46168061+OliverRetro@users.noreply.github.com>
2025-08-09 15:09:45 +0300

    Sound Settings (#12)

---
+++ src/Skylight.Protocol/Packets/Incoming/Preferences/ISetSoundSettingsIncomingPacket.cs
@@ -0,0 +1,8 @@
+﻿namespace Skylight.Protocol.Packets.Incoming.Preferences;
+
+public interface ISetSoundSettingsIncomingPacket : IGameIncomingPacket
+{
+	public int UiVolume { get; }
+	public int FurniVolume { get; }
+	public int TraxVolume { get; }
+}
